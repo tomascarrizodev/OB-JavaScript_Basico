79a2018a3acc826bc184f2c24202e64ff3215df8
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-11-15 13:06:04 -0300

    sintaxis

---
+++ 2.-Sintaxis-variables/index.js
@@ -0,0 +1,12 @@
+const yo = [
+    'Tomás',
+    23,
+    true,
+    new Date(1999, 10, 5),
+    {
+        título: 'De la Tierra a la Luna',
+        autor: 'Julio Verne',
+        fecha: new Date(1865),
+        url: 'https://articulo.mercadolibre.com.ar/MLA-876403913-de-la-tierra-a-la-luna-_JM#is_advertising=true&position=2&search_layout=stack&type=pad&tracking_id=7fdba56b-1fbf-415d-9c99-22bb1faa018f&is_advertising=true&ad_domain=VQCATCORE_LST&ad_position=2&ad_click_id=ODQ5YTk0MjQtYTdlZi00ZTg5LTg3YzEtYWQ0MWQ3NThmZTFm'
+    }
+]
