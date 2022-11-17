d7fbe6d91dc4cd449c9aa8d6fffed5adf248c770
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-11-16 21:23:50 -0300

    strings

---
+++ 5.-Numeros-en-JS/numeros.js
@@ -0,0 +1,11 @@
+const alturaCm = 175;
+
+const alturaM = 1.75;
+
+const pesoKg = 70.5;
+
+const alturaRedondeada = Math.ceil(alturaM);
+
+const pesoRedondeado = Math.floor(pesoKg);
+
+const maximoValor = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
