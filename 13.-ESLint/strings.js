421fa51a0a3ab2562cb6df63cbfe9acd4a1daec9
tomascarrizodev <tomascarrizo.dev@gmail.com>
2022-11-24 19:17:47 -0300

    eslint

---
+++ 13.-ESLint/strings.js
@@ -0,0 +1,19 @@
+const nombre = "Tomás";
+
+const apellido = "Carrizo";
+
+const estudiante = `${nombre} ${apellido}`;
+
+const estudianteMayus = estudiante.toUpperCase();
+
+const estudianteMinus = estudiante.toLowerCase();
+
+const numeroDeLetras = estudiante.length;
+
+const primeraLetraNombre = nombre.at(0);
+
+const ultimaLetraApellido = estudiante.at(numeroDeLetras - 1);
+
+const sinEspacios = estudiante.replace(/ /g, "");
+
+const bool = estudiante.includes(nombre);
